a2c9f2d6b58bc5df7b96388a3a46e12a07d3ea6b
Adithya Kashyap <119522695+adithyakashyapv@users.noreply.github.com>
2023-12-23 17:54:49 +0530

    Update the main to combine the outputs
    Updating the main file to handle the missing data and combine all the files and create a single csv file

--- get_ticker_data.py
+++ get_ticker_data.py
@@ -9,11 +9,16 @@ import s3fs
 import requests
 import time
 import os
+import handleMissingData
+import combineData
 
 api_key = api_keys.api_key
 key_index = 0
 
+columns = {'Unnamed: 0':'date' ,'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'}
+
 def get_data(symbol):
+    symbol_raw = symbol
     symbol = symbol+".BSE"
     global key_index
     key = api_key[key_index]
@@ -29,7 +34,12 @@ def get_data(symbol):
     if 'Time Series (Daily)' in data:
         # Process and store the data (example: write to CSV)
         df = pd.DataFrame(data['Time Series (Daily)']).T
-        df.to_csv(f'data//{symbol}_stock_data.csv')
+        df.to_csv(f'data//{symbol_raw}_stock_data.csv')
+        df = pd.read_csv(f'data//{symbol_raw}_stock_data.csv')
+        df.rename(columns=columns, inplace=True)
+        data = handleMissingData.fill_missing_values(df)
+        data.to_csv(f'data//{symbol_raw}_stock_data.csv', index=False)
+
 
     if (key_index + 1) % 25 == 0:
         key_index = (key_index + 1) % len(key)
@@ -44,6 +54,8 @@ def main():
     for symbol in ticker_symbols:
         get_data(symbol)
 
+    combineData.combine_data('data')
+    
 if __name__ == "__main__":
     main()
 
